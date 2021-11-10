ebdb8f2fb5474187794525cf6dd9c21b62db607b
vg654 <vg2494@columbia.edu>
2021-11-10 12:07:17 -0500

    adding test
    Added a test function to the test folder of package & added pytest as a dev dependency

--- cipher_vg2494/tests/test_cipher_vg2494.py
+++ cipher_vg2494/tests/test_cipher_vg2494.py
@@ -1,1 +1,9 @@
 from cipher_vg2494 import cipher_vg2494
+
+import pytest
+
+def test_cipher_with_single_word():
+    example = "Potato"
+    expected = "Srwdwr"
+    result = cipher(example, 3, encrypt = True)
+    assert result == expected
